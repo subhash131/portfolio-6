a21158d0942de42412e4bd7ac4df09423e78445a
subhash131 <subhashnayak131@gmail.com>
2024-01-17 01:15:48 +0530

    add digital product section

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,17 @@ import Perspective3d from "@/components/perspective_3d/Perspective3d";
 import Background from "@/components/background/Background";
 import TitleBar from "@/components/titlebar/TitleBar";
 import WebDeveloper from "@/components/web_developer/WebDeveloper";
+import DigitalProduct from "@/components/digital_product/DigitalProduct";
+import ImmersiveExperience from "@/components/immersive_experience/ImmersiveExperience";
 
 export default function Home() {
   return (
-    <Perspective3d className="h-screen w-screen overflow-hidden bg-black grid place-content-center">
+    <Perspective3d className="h-screen w-screen overflow-hidden bg-black grid place-content-center select-none">
       <Background className="bg-[#181E12] p-2">
         <TitleBar />
         <WebDeveloper />
+        <DigitalProduct />
+        <ImmersiveExperience />
       </Background>
     </Perspective3d>
   );
---
+++ components/digital_product/DigitalProduct.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Perspective3d from "../perspective_3d/Perspective3d";
+import { englebert } from "../fonts";
+
+const DigitalProduct = () => {
+  return (
+    <Perspective3d
+      className={`bottom-10 absolute text-2xl text-center text-white left-[calc(24rem+5rem)] w-80 p-4 h-60 bg-[#0008D3] grid place-content-center rounded-md transition-all ${englebert.className}`}
+    >
+      <div className="w-3 h-3 top-1 left-1 rounded-full bg-black absolute"></div>
+      <div className="w-3 h-3 top-1 right-1 rounded-full bg-black absolute "></div>
+      <Perspective3d>
+        <p>
+          I combine design, technology, and strategy to create tailored digital
+          solutions that dirves rapid, sustainable growth for your brand
+        </p>
+      </Perspective3d>
+    </Perspective3d>
+  );
+};
+
+export default DigitalProduct;
