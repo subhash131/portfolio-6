3e6282a650d1005ba530418283f96a8c8812b22e
subhash131 <subhashnayak131@gmail.com>
2024-01-17 01:52:36 +0530

    update titlebar

--- components/titlebar/TitleBar.tsx
+++ components/titlebar/TitleBar.tsx
@@ -5,9 +5,9 @@ const Titlebar = () => {
   return (
     <Perspective3d className="w-full h-10 bg-[#2F2F2F] rounded flex items-center justify-between p-4">
       <Perspective3d className="flex gap-2 ">
-        <div className="w-3 h-3 rounded-full bg-[#666666]" />
-        <div className="w-3 h-3 rounded-full bg-[#666666]" />
-        <div className="w-3 h-3 rounded-full bg-[#666666]" />
+        <div className="w-3 h-3 rounded-full bg-[#F75745]" />
+        <div className="w-3 h-3 rounded-full bg-[#FAB93B]" />
+        <div className="w-3 h-3 rounded-full bg-[#44B041]" />
       </Perspective3d>
     </Perspective3d>
   );
