590fd2d3a0866b1d6f1aa72e4e94acb9c0d998a9
subhash131 <subhashnayak131@gmail.com>
2024-01-17 01:37:50 +0530

    add immersive exp component

--- components/immersive_experience/ImmersiveExperience.tsx
+++ components/immersive_experience/ImmersiveExperience.tsx
@@ -1,8 +1,21 @@
 import React from "react";
 import Perspective3d from "../perspective_3d/Perspective3d";
+import { englebert } from "../fonts";
 
 const ImmersiveExperience = () => {
-  return <Perspective3d>ImmersiveExperience</Perspective3d>;
+  return (
+    <Perspective3d
+      className={`top-20 absolute text-xl text-center text-black left-[calc(24rem+5rem)] w-80 p-4 h-52 bg-[#C4BDA5] grid place-content-center rounded-md transition-all ${englebert.className}`}
+    >
+      <div className="w-3 h-3 top-1 left-1 rounded-full bg-black absolute"></div>
+      <div className="w-3 h-3 top-1 right-1 rounded-full bg-black absolute "></div>
+      <Perspective3d translatez={40}>
+        For any digital product, a seemless and enhanced user experience(UX) is
+        crucial, accompained by an emotionally engaging and immersive user
+        interface(UI)
+      </Perspective3d>
+    </Perspective3d>
+  );
 };
 
 export default ImmersiveExperience;
