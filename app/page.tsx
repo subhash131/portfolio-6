dfe9b58d24f6a104388aae0485e5bcf1a878a4a3
subhash131 <subhashnayak131@gmail.com>
2024-01-17 00:13:33 +0530

    initial setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,14 @@
-import Perspective3d from "@/components/3dPerspective/Perspective3d";
+import Perspective3d from "@/components/perspective_3d/Perspective3d";
 import Background from "@/components/background/Background";
+import TitleBar from "@/components/titlebar/TitleBar";
+import WebDeveloper from "@/components/web_developer/WebDeveloper";
 
 export default function Home() {
   return (
     <Perspective3d className="h-screen w-screen overflow-hidden bg-black grid place-content-center">
       <Background className="bg-[#181E12] p-2">
-        <Perspective3d className="w-full h-10 bg-[#2F2F2F] rounded flex items-center p-4">
-          <div className="flex gap-2">
-            <Perspective3d className="w-3 h-3 rounded-full bg-[#666666]"></Perspective3d>
-            <Perspective3d className="w-3 h-3 rounded-full bg-[#666666]"></Perspective3d>
-            <Perspective3d className="w-3 h-3 rounded-full bg-[#666666]"></Perspective3d>
-          </div>
-        </Perspective3d>
+        <TitleBar />
+        <WebDeveloper />
       </Background>
     </Perspective3d>
   );
