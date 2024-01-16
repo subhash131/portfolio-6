6a1e79f8df9a972cdb81f76f6fb58b7aacaeef82
subhash131 <subhashnayak131@gmail.com>
2024-01-17 01:16:50 +0530

    Create immersive exp component

---
+++ components/immersive_experience/ImmersiveExperience.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Perspective3d from "../perspective_3d/Perspective3d";
+
+const ImmersiveExperience = () => {
+  return <Perspective3d>ImmersiveExperience</Perspective3d>;
+};
+
+export default ImmersiveExperience;
