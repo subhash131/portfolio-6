85666b51c355b6539ab839258ffe95de0f2f76f5
subhash131 <subhashnayak131@gmail.com>
2024-01-17 01:38:24 +0530

    update transform dynamically

--- components/perspective_3d/Perspective3d.tsx
+++ components/perspective_3d/Perspective3d.tsx
@@ -3,14 +3,16 @@ import React from "react";
 const Perspective3d = ({
   children,
   className,
+  translatez = 75,
 }: {
   children?: React.ReactNode;
   className?: string;
+  translatez?: number;
 }) => {
   return (
     <div
       style={{
-        transform: "translateZ(75px)",
+        transform: `translateZ(${translatez}px)`,
         transformStyle: "preserve-3d",
       }}
       className={className}
