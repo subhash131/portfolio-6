cd908786ce91f92545f3b288d81a3aad119c5fdf
subhash131 <subhashnayak131@gmail.com>
2024-01-17 01:38:03 +0530

    update title color

--- components/navbar/TitleBar.tsx
+++ components/navbar/TitleBar.tsx
@@ -5,9 +5,18 @@ const Navbar = () => {
   return (
     <Perspective3d className="w-full h-10 bg-[#2F2F2F] rounded flex items-center p-4">
       <div className="flex gap-2">
-        <Perspective3d className="w-3 h-3 rounded-full bg-[#666666]" />
-        <Perspective3d className="w-3 h-3 rounded-full bg-[#666666]" />
-        <Perspective3d className="w-3 h-3 rounded-full bg-[#666666]" />
+        <Perspective3d
+          translatez={25}
+          className="w-3 h-3 rounded-full bg-[#F75745]"
+        />
+        <Perspective3d
+          translatez={25}
+          className="w-3 h-3 rounded-full bg-[#FAB93B]"
+        />
+        <Perspective3d
+          translatez={25}
+          className="w-3 h-3 rounded-full bg-[#44B041]"
+        />
       </div>
     </Perspective3d>
   );
