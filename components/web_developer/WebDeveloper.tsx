aa2db3eda672f4cefed2ce23a05ff460ad21cab6
subhash131 <subhashnayak131@gmail.com>
2024-01-17 01:17:02 +0530

    add web developement section

--- components/web_developer/WebDeveloper.tsx
+++ components/web_developer/WebDeveloper.tsx
@@ -1,24 +1,19 @@
 import React from "react";
 import Perspective3d from "../perspective_3d/Perspective3d";
-import { Englebert } from "next/font/google";
-
-const englebert = Englebert({
-  weight: ["400"],
-  subsets: ["latin"],
-});
+import { englebert } from "../fonts";
 
 const WebDeveloper = () => {
   return (
     <Perspective3d
-      className={`bottom-4 absolute text-3xl text-center text-white left-4 w-96 h-64 bg-[#7B896B] grid place-content-center rounded-md ${englebert.className} transition-all`}
+      className={`bottom-10 absolute text-3xl text-center text-white left-10 w-96 h-80 bg-[#7B896B] grid place-content-center rounded-md ${englebert.className} transition-all`}
     >
-      <Perspective3d className="w-32 h-32 rounded-full bg-black">
-        <Perspective3d className="-ml-28 -mt-1 w-auto">
+      <Perspective3d className="w-52 h-52 rounded-full bg-black">
+        <Perspective3d className="-ml-28 mt-10 w-auto">
           Web Developer
         </Perspective3d>
-        <Perspective3d className="ml-6 mt-10  w-52 text-sm">
-          The combination of my passion for design, code & interaction positions
-          me in a unique place in the web design world.
+        <Perspective3d className="ml-12 mt-16 w-52 text-sm">
+          The combination of my passion for design, code & interaction,
+          positions me in a unique place in the web design world.
         </Perspective3d>
       </Perspective3d>
     </Perspective3d>
