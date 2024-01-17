4f5dd8ecd8ff38c30222797eac4917a4791ba46b
subhash131 <subhashnayak131@gmail.com>
2024-01-17 22:42:11 +0530

    fix components

--- components/nails/Nail.tsx
+++ components/nails/Nail.tsx
@@ -3,18 +3,18 @@ import React from "react";
 const Nail4 = () => {
   return (
     <>
-      <div className="w-[6%] h-[2%] top-1 left-1 rounded-full bg-black absolute"></div>
-      <div className="w-[6%] h-[2%] top-1 right-1 rounded-full bg-black absolute "></div>
-      <div className="w-[6%] h-[2%] bottom-1 left-1 rounded-full bg-black absolute"></div>
-      <div className="w-[6%] h-[2%] bottom-1 right-1 rounded-full bg-black absolute "></div>
+      <div className="w-[6%] h-[2%] top-1 left-1 rounded-full bg-black absolute" />
+      <div className="w-[6%] h-[2%] top-1 right-1 rounded-full bg-black absolute " />
+      <div className="w-[6%] h-[2%] bottom-1 left-1 rounded-full bg-black absolute" />
+      <div className="w-[6%] h-[2%] bottom-1 right-1 rounded-full bg-black absolute " />
     </>
   );
 };
 export const Nail2 = () => {
   return (
     <>
-      <div className="w-[3%] h-[5%] top-1 left-1 rounded-full bg-black absolute"></div>
-      <div className="w-[3%] h-[5%] top-1 right-1 rounded-full bg-black absolute "></div>
+      <div className="w-[3%] h-[5%] top-1 left-1 rounded-full bg-black absolute" />
+      <div className="w-[3%] h-[5%] top-1 right-1 rounded-full bg-black absolute " />
     </>
   );
 };
