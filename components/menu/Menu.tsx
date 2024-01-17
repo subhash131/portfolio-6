45e47df646ecf186012507f7f9f41a572de63205
subhash131 <subhashnayak131@gmail.com>
2024-01-17 22:42:31 +0530

    update style: pointer events

--- components/menu/Menu.tsx
+++ components/menu/Menu.tsx
@@ -10,24 +10,22 @@ const Menu = () => {
   return (
     <Perspective3d
       translatez={100}
-      className={`top-[12%] absolute text-xl text-center text-white left-[83%] w-[13%] h-[85%] p-4 bg-[#a2652f] grid place-content-center gap-4 rounded-md transition-all ${englebert.className}`}
+      className={`top-[12%] absolute text-xl text-center text-white left-[83%] w-[13%] h-[85%] p-4 bg-[#a2652f] grid place-content-center gap-4 rounded-md transition-all pointer-events-none ${englebert.className}`}
     >
       <Nail4 />
-      <Perspective3d translatez={50} className="text-black pointer-events-none">
+      <Perspective3d translatez={50} className="text-black">
         Menu
       </Perspective3d>
       {brands.map((item, i) => {
         return (
           <Perspective3d
             key={`${item}-${i}`}
-            className="w-28 h-28 rounded-md bg-[#000] grid place-content-center shadow-2xl cursor-pointer hover:bg-[#4c4c4a] transition-all"
+            className="w-28 h-28 rounded-md bg-[#000] grid place-content-center shadow-2xl cursor-pointer hover:bg-[#4c4c4a] transition-all pointer-events-auto"
             onClick={() => {
               console.log("hi");
             }}
           >
-            <Perspective3d className="pointer-events-none">
-              {item}
-            </Perspective3d>
+            <Perspective3d>{item}</Perspective3d>
           </Perspective3d>
         );
       })}
