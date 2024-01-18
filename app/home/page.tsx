921d0d9ce8712204ca4256290a419f0693c68e33
subhash131 <subhashnayak131@gmail.com>
2024-01-18 23:56:03 +0530

    update imports

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,7 +1,7 @@
-import DigitalProduct from "@/components/digital_product/DigitalProduct";
-import ImmersiveExperience from "@/components/immersive_experience/ImmersiveExperience";
-import WebDeveloper from "@/components/web_developer/WebDeveloper";
-import WorkedWith from "@/components/worked_with/WorkedWith";
+import DigitalProduct from "@/components/home/digital_product/DigitalProduct";
+import ImmersiveExperience from "@/components/home/immersive_experience/ImmersiveExperience";
+import WebDeveloper from "@/components/home/web_developer/WebDeveloper";
+import WorkedWith from "@/components/home/worked_with/WorkedWith";
 import React from "react";
 
 const Home = () => {
