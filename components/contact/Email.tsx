9d79db8c529bfae25656a3f705e55fad92214204
subhash131 <subhashnayak131@gmail.com>
2024-01-19 02:24:53 +0530

    create email component

---
+++ components/contact/Email.tsx
@@ -0,0 +1,34 @@
+"use client";
+import React from "react";
+import MotionPerspective3d from "../common/perserve_3d/MotionPerspective3d";
+import { englebert } from "@/fonts";
+
+const Email = () => {
+  return (
+    <MotionPerspective3d
+      initial={{
+        opacity: 0,
+        skewY: 0,
+      }}
+      animate={{
+        opacity: 1,
+        skewY: -2,
+      }}
+      translatez={60}
+      transition={{ duration: 0.3, delay: 0.5, ease: "linear" }}
+      className={` absolute text-2xl top-28 h-[10%] text-center bg-white left-[5%] p-4 w-[65%] rounded-md transition-all flex gap-10 flex-col ${englebert.className}  `}
+    >
+      <input
+        style={{
+          transformStyle: "preserve-3d",
+        }}
+        type="email"
+        required
+        placeholder="Drop your email..."
+        className="pointer-events-auto border-b ring-0 outline-none  w-full h-10 resize-none"
+      />
+    </MotionPerspective3d>
+  );
+};
+
+export default Email;
