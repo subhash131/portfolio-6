74843b063a11c565ccd6f87f03673e129747d3f5
subhash131 <subhashnayak131@gmail.com>
2024-01-19 00:20:44 +0530

    add component contact

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,31 @@
+"use client";
+import MotionPerspective3d from "@/components/common/perserve_3d/MotionPerspective3d";
+import Perspective3d from "@/components/common/perserve_3d/Perspective3d";
+import { englebert } from "@/fonts";
 import React from "react";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <Perspective3d
+      className="w-[80%] h-full flex justify-center items-center"
+      translatez={0}
+    >
+      <MotionPerspective3d
+        initial={{
+          height: 0,
+          fontSize: "0rem",
+          visibility: "hidden",
+        }}
+        animate={{
+          height: "80%",
+          fontSize: "1.5rem",
+          visibility: "visible",
+        }}
+        transition={{ duration: 0.3, delay: 0.2 }}
+        className={` absolute text-2xl text-center bg-white text-white left-[2%] p-4 w-[75%] grid place-content-center rounded-md transition-all ${englebert.className} pointer-events-none`}
+      ></MotionPerspective3d>
+    </Perspective3d>
+  );
 };
 
 export default Contact;
