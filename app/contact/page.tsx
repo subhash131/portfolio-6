87d7e1c5416fab4928a6f79382abaec6d5411dde
subhash131 <subhashnayak131@gmail.com>
2024-01-19 02:23:30 +0530

    add contact component

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,29 +1,19 @@
 "use client";
-import MotionPerspective3d from "@/components/common/perserve_3d/MotionPerspective3d";
 import Perspective3d from "@/components/common/perserve_3d/Perspective3d";
-import { englebert } from "@/fonts";
+import DropMessage from "@/components/contact/DropMessage";
+import Email from "@/components/contact/Email";
+import SendMessage from "@/components/contact/Send";
 import React from "react";
 
 const Contact = () => {
   return (
     <Perspective3d
-      className="w-[80%] h-full flex justify-center items-center"
-      translatez={0}
+      className="w-[85%] h-full flex justify-center items-center text-gray-600"
+      translatez={10}
     >
-      <MotionPerspective3d
-        initial={{
-          height: 0,
-          fontSize: "0rem",
-          visibility: "hidden",
-        }}
-        animate={{
-          height: "80%",
-          fontSize: "1.5rem",
-          visibility: "visible",
-        }}
-        transition={{ duration: 0.3, delay: 0.2 }}
-        className={` absolute text-2xl text-center bg-white text-white left-[2%] p-4 w-[75%] grid place-content-center rounded-md transition-all ${englebert.className} pointer-events-none`}
-      ></MotionPerspective3d>
+      <Email />
+      <DropMessage />
+      <SendMessage />
     </Perspective3d>
   );
 };
