b922e1f2705483ca787c476686850d69705ec7ea
subhash131 <subhashnayak131@gmail.com>
2024-01-19 02:28:10 +0530

    create currpage and page color states

--- context/StateContext.tsx
+++ context/StateContext.tsx
@@ -1,11 +1,36 @@
 "use client";
-import React, { createContext, useContext } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
 
-export const StateContext = createContext<null | { a: string }>(null);
+export const StateContext = createContext<any>(null);
 
 const StateContextProvider = ({ children }: { children: React.ReactNode }) => {
+  const [currentPage, setCurrentPage] = useState<
+    "projects" | "contact" | "home"
+  >("home");
+  const [bgColor, setBgColor] = useState<"#1C1023" | "#111A0B" | "#092A34">(
+    "#111A0B"
+  );
+  const getPageColor = () => {
+    switch (currentPage) {
+      case "home":
+        setBgColor("#111A0B");
+        break;
+      case "contact":
+        setBgColor("#1C1023");
+        break;
+      case "projects":
+        setBgColor("#092A34");
+        break;
+      default:
+        setBgColor("#111A0B");
+    }
+  };
+  useEffect(() => {
+    getPageColor();
+  }, [currentPage]);
+
   return (
-    <StateContext.Provider value={{ a: "aaa" }}>
+    <StateContext.Provider value={{ currentPage, bgColor, setCurrentPage }}>
       {children}
     </StateContext.Provider>
   );
