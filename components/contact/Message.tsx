790ec92781eff5db4535215e7bae258149f44b17
subhash131 <subhashnayak131@gmail.com>
2024-01-19 21:42:00 +0530

    update  animation

--- components/contact/Message.tsx
+++ components/contact/Message.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import MotionPerspective3d from "../common/perserve_3d/MotionPerspective3d";
+import Perspective3d from "../common/perserve_3d/Perspective3d";
 
 const Message = () => {
   return (
@@ -18,13 +19,17 @@ const Message = () => {
       transition={{ duration: 0.3, delay: 0.2 }}
       className={` bottom-12 absolute text-2xl text-center bg-white  left-[5%] p-4 w-[65%] rounded-md transition-all flex gap-10 flex-col pointer-events-none`}
     >
-      <textarea
-        style={{
-          transformStyle: "preserve-3d",
-        }}
-        placeholder="Type message..."
-        className="pointer-events-auto border-b ring-0 outline-none  w-full h-full resize-none"
-      />
+      <Perspective3d className="w-full h-full">
+        <textarea
+          style={{
+            transformStyle: "preserve-3d",
+            transform: "translateZ(35px)",
+          }}
+          placeholder="Type message..."
+          className="pointer-events-auto border-b ring-0 bg-transparent outline-none w-full h-full resize-none drop-shadow-md"
+          autoFocus
+        />
+      </Perspective3d>
     </MotionPerspective3d>
   );
 };
