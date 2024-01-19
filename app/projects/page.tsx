45401e8d247364c6309b9ca9213533f6b83096f0
subhash131 <subhashnayak131@gmail.com>
2024-01-19 22:04:04 +0530

    intiial project page setup

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,7 +1,18 @@
+import Perspective3d from "@/components/common/perserve_3d/Perspective3d";
+import Image from "next/image";
 import React from "react";
 
 const Projects = () => {
-  return <div></div>;
+  return (
+    <Perspective3d>
+      <Image
+        src="/assets/project1.png"
+        width={100}
+        height={100}
+        alt="project 1"
+      />
+    </Perspective3d>
+  );
 };
 
 export default Projects;
